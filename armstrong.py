ca4305c61a8febedcf14aa2dffa4f1b383644d3c
VacseezX <c2vacger@vasvari.org>
2025-12-15 09:44:22 +0100

    jelszoerosseg

--- armstrong.py
+++ armstrong.py
@@ -23,3 +23,40 @@ class TestArmstrong(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+##------------------------------------------------------##
+##------------------------------------------------------##
+
+def jelszo_erosseg(s: str):
+    erosseg = 1
+    nemjo = "jelszo"
+    nemjoszam = "123"
+    if nemjo not in s and nemjoszam not in s or len(s) == 0:
+        if len(s) >= 5:
+            erosseg += 1
+            if len(s) >= 8:
+                erosseg += 2
+        for char in s:
+            if char == "_" or char == "-" or char == ".":
+                erosseg += 2
+    else:
+        erosseg = 0
+    return erosseg
+
+jelszo = input("Input: ")
+print(f"Return: {jelszo_erosseg(jelszo)}")
+
+class TestJelszoErosseg(unittest.TestCase):
+    
+    def test_jelszo_mukszik(self):
+        self.assertEqual(jelszo_erosseg("hazi_macska_4_life"), 10)
+    
+    def test_jelso_works(self):
+        self.assertEqual(jelszo_erosseg("ez1feltorhetetlenjelszo"), 0)
+    
+if __name__ == '__main__':
+    unittest.main()
+
+##------------------------------------------------------##
+##------------------------------------------------------##
+
